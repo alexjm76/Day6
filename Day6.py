e2e833a2e619c10ace1b8e0f728562bad200d6f4
Jungmoo Yoon <alexjm@naver.com>
2023-01-19 11:22:13 +0900

    global

--- Day6.py
+++ Day6.py
@@ -1,26 +1,20 @@
-
-def document_info(func):
-    def new_function(*args, **kwargs):
-        print("Running function: " ,func.__name__)  # 함수의 이름 리턴
-        print("Positional arguments: ", args)  # 튜플출력
-        print("Keyword arguments:", kwargs)  # 딕셔너리 출력
-        result = func(*args, **kwargs)
-        print("Result:" ,result)
-        return result
-
-    return new_function()
+animal = "fruitbat"
 
 
-@document_info
-def sub_int(x, y):
-    return x - y
+def print_global():
+    print("inside print_global:", animal)
 
 
-@document_info
-def squares(n):
-    return n * n
+print("at the top level:", animal)
+print(globals())
 
+g=1
 
-print(sub_int(7, 3))
-print(squares5
-)
+def print_global():
+    global g
+    print(g)
+    g=2
+    print(g)
+
+print(g)
+print_global()
