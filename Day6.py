b1deebe89c469b36e795a05ed418a365ca323a72
Jungmoo Yoon <alexjm@naver.com>
2023-01-19 10:28:59 +0900

    genrator ëand decorator

--- Day6.py
+++ Day6.py
@@ -1,19 +1,16 @@
-#generator
+# def document_it(func):
+#     def new_function(*args, **kwargs):
+#         print("Running function: ",func.__name__) #함수의 이름 리턴
+#         print("Positional arguments: ", args)  #튜플출력
+#         print("Keyword arguments:" kwargs)  #딕셔너리 출력
+#         result = func(*args, **kwargs)
+#         print("Result:" ,result)
+#         return result
+#     return new_function()
 
-def a(): #generator
-    yield 1
-    yield 2
-    yield 3
 
+@document_it
+def add_ints(a,b):
+    return  a+b
 
-def b(): #normal function
-    return 1 #여기서 끝남
-    return 2
-    return 3
-
-print(a(),b())
-c=a()
-print(c)
-for i in a():
-    print(i)
-
+add_ints(3,5)  #기존의 함수를 건들이지 않으면서 수정하고 싶을때 decorator 사용
