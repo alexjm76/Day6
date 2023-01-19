a6f0339cc2fecbbc5d2b2199b33bfe30e42221d9
Jungmoo Yoon <alexjm@naver.com>
2023-01-19 10:45:25 +0900

    genrator2

--- Day6.py
+++ Day6.py
@@ -1,16 +1,26 @@
-# def document_it(func):
-#     def new_function(*args, **kwargs):
-#         print("Running function: ",func.__name__) #함수의 이름 리턴
-#         print("Positional arguments: ", args)  #튜플출력
-#         print("Keyword arguments:" kwargs)  #딕셔너리 출력
-#         result = func(*args, **kwargs)
-#         print("Result:" ,result)
-#         return result
-#     return new_function()
+
+def document_info(func):
+    def new_function(*args, **kwargs):
+        print("Running function: " ,func.__name__)  # 함수의 이름 리턴
+        print("Positional arguments: ", args)  # 튜플출력
+        print("Keyword arguments:", kwargs)  # 딕셔너리 출력
+        result = func(*args, **kwargs)
+        print("Result:" ,result)
+        return result
+
+    return new_function()
 
 
-@document_it
-def add_ints(a,b):
-    return  a+b
+@document_info
+def sub_int(x, y):
+    return x - y
 
-add_ints(3,5)  #기존의 함수를 건들이지 않으면서 수정하고 싶을때 decorator 사용
+
+@document_info
+def squares(n):
+    return n * n
+
+
+print(sub_int(7, 3))
+print(squares5
+)
